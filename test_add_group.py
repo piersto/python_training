818d28d7f0f6d48769703f4e5f45d545d187050b
Pierre <pstoiko@gmail.com>
2018-11-02 08:58:29 -0400

    Задание №4: Перевести тесты с unittest на py.test и реализовать фикстуры для теста

--- test_add_group.py
+++ test_add_group.py
@@ -6,7 +6,7 @@ from application import Application
 
 @pytest.fixture
 def app(request):
-    fixture = Application
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
