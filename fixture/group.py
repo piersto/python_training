ee562680493022f4ab06673be58be4e1b5eab812
Pierre <pstoiko@gmail.com>
2018-11-04 12:17:20 -0500

    Задание №6: Сделать фикстуру общей для всех тестов(without add contact)

--- fixture/group.py
+++ fixture/group.py
@@ -24,6 +24,20 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # delete first group
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
+
+
+
+
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
