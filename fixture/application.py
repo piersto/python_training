854e6f20e98f4ae8c721953f28e49b75b260c7f5
Pierre <pstoiko@gmail.com>
2018-11-10 21:17:06 -0500

    Not working test_delete_contact with wd.switch_to_alert().accept()

--- fixture/application.py
+++ fixture/application.py
@@ -140,6 +140,7 @@ class Application:
         wd = self.wd
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
         self.assertRegexpMatches(self.close_alert_and_get_its_text(), r"^Delete 1 addresses[\s\S]$")
 
 
