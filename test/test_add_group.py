78ab6d153959a7a4dcba0ba3bf7ad6fc0021854d
Pierre <pstoiko@gmail.com>
2018-11-11 14:54:47 -0500

    Задание №8: Реализовать проверку валидности фикстуры и проверку состояния сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,6 +5,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="Group 1", header="group 1 -1", footer="group1-1-1"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
