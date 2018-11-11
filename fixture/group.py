f8599c8458437cbdbdd8e9d84f9c6fbdc626ba47
Pierre <pstoiko@gmail.com>
2018-11-11 12:01:56 -0500

    test_modify_group_name test_modify_group_header

--- fixture/group.py
+++ fixture/group.py
@@ -22,14 +22,17 @@ class GroupHelper:
     def fill_group_form(self, group):
         # fill group form
         wd = self.app.wd
-        if group.name is not None:
-            wd.find_element_by_name("group_name").click()
-            wd.find_element_by_name("group_name").clear()
-            wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.name)
+        self.change_field_value("group_footer", group.name)
+
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def open_groups_page(self):
         wd = self.app.wd
@@ -74,7 +77,6 @@ class GroupHelper:
         self.fill_group_form(new_group_data)
         # submit modification
         wd.find_element_by_name("update").click()
-
         self.return_to_group_page()
 
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,20 +2,12 @@
 
 from model.group import Group
 
-
-def test_add_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="New group"))
     app.session.logout()
 
-
-
-
-
-
-
-
-#def test_add_empty_group(app):
- #   app.session.login(username="admin", password="secret")
-  #  app.group.create(Group(name="", header="", footer=""))
- #   app.session.logout()
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
+    app.session.logout()
