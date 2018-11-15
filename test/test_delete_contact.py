c6c6effdec4ea0a9df83d836a864b9e731a0a442
Pierre <pstoiko@gmail.com>
2018-11-14 20:31:39 -0500

    Повышаем "интеллектуальность" тестов: обеспечение выполнения предусловий - test_delete_group, test_delete_contact проходит

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -4,4 +4,8 @@ from model.add_new_contact import Contact
 
 def test_delete_contact(app):
     app.go_to_home_page()
+    if app.delete_contact.count_contacts() == 0:
+        app.add_user(Contact(firstname="Vasia"))
+        app.submit_specified_user()
+        app.go_to_home_page()
     app.delete_contact.del_contact()
